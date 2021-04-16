53f2f9e874f3d8faa7318195b8de527b55f446a9
Rodrigo Alejandro Viteri Velez <rodrigo.viteri.velez@estudiantes.cordillera.edu.ec>
2021-04-16 00:10:27 -0500

    estadisticas y estado de cita

--- Vistas/Inicio/contenido_vista.php
+++ Vistas/Inicio/contenido_vista.php
@@ -1,8 +1,94 @@
 <?php
 session_start();
-include '../../plantilla/header.php'; ?>
-<body>	
+include_once '../../plantilla/header.php';
 
+
+
+
+
+?>
+
+
+<body>	
+            <div id="page-inner">
+
+			<div class="dashboard-cards"> 
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+						<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image red">
+						<i class="material-icons dp48">import_export</i>
+						</div>
+						<div class="card-stacked red">
+						<div class="card-content">
+						<h3><?php 
+						$sql = "select COUNT(*) from tbl_paciente ";
+$result = $co->query($sql);//$pdo sería el objeto conexión
+$total = $result->fetchColumn(); echo $total 
+?></h3> 
+						</div>
+						<div class="card-action">
+						<strong>Pacientes Registrados</strong>
+						</div>
+						</div>
+						</div>
+	 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+						<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image orange">
+						<i class="material-icons dp48">shopping_cart</i>
+						</div>
+						<div class="card-stacked orange">
+						<div class="card-content">
+						<h3><?php $sql1 = "select COUNT(*) from tbl_paciente ";
+$result1 = $co->query($sql1);//$pdo sería el objeto conexión
+$citas = $result1->fetchColumn(); echo $citas?></h3> 
+						</div>
+						<div class="card-action">
+						<strong>Citas Agendadas</strong>
+						</div>
+						</div>
+						</div> 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+							<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image blue">
+						<i class="material-icons dp48">equalizer</i>
+						</div>
+						<div class="card-stacked blue">
+						<div class="card-content">
+						<h3>24,225</h3> 
+						</div>
+						<div class="card-action">
+						<strong>PRODUCTS</strong>
+						</div>
+						</div>
+						</div> 
+						 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+					<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image green">
+						<i class="material-icons dp48">supervisor_account</i>
+						</div>
+						<div class="card-stacked green">
+						<div class="card-content">
+						<h3>88,658</h3> 
+						</div>
+						<div class="card-action">
+						<strong>VISITS</strong>
+						</div>
+						</div>
+						</div> 
+						 
+                    </div>
+                </div>
+			   </div>
 <div class="form-group row">
 		<img src="../../img/icono.svg" width="40%" style="padding-left: 10%;">
 		<div class="col-sm-6" style="padding-left: 10%;">
