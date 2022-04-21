bd5f93e68436431124a64be5c12a202354d7c9fd
Edisson998 <cerdito1998@hotmail.com>
2022-04-21 16:33:18 -0500

    URL

--- Controlador/GlobalFuntion.php
+++ Controlador/GlobalFuntion.php
@@ -5,7 +5,7 @@
         $host = $_SERVER['HTTP_HOST'];
         
         //Colocar como aparece en el navegador
-        const SERVERURL= "https://citasmedicas-app.herokuapp.com/";
+        const SERVERURL= "https://az-citasmedicas.azurewebsites.net/";
         
 
     
